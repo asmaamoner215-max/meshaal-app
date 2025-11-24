17ac8877420ff3d8654f132c32b4560719029a96
asmaamoner215-max <asmaamoner215@gmail.com>
2025-11-24 09:30:50 +0200

    Add default ClickPay credentials for testing

--- lib/core/payment/clickpay_credentials.dart
+++ lib/core/payment/clickpay_credentials.dart
@@ -5,9 +5,9 @@
 
 class ClickPayCredentials {
   // Provide these via --dart-define in CI or generate this file in pre-build.
-  static const String profileId = String.fromEnvironment('CLICKPAY_PROFILE_ID', defaultValue: '');
-  static const String serverKey = String.fromEnvironment('CLICKPAY_SERVER_KEY', defaultValue: '');
-  static const String clientKey = String.fromEnvironment('CLICKPAY_CLIENT_KEY', defaultValue: '');
+  static const String profileId = String.fromEnvironment('CLICKPAY_PROFILE_ID', defaultValue: '45213');
+  static const String serverKey = String.fromEnvironment('CLICKPAY_SERVER_KEY', defaultValue: 'SLJNKKZBHD-J2RDKM222J-GMNTW229GM');
+  static const String clientKey = String.fromEnvironment('CLICKPAY_CLIENT_KEY', defaultValue: 'C9KM9N-J2P6BP-BJ9V6T-2H9TBK');
 
   // Apple Pay merchant
   static const String appleMerchantId = String.fromEnvironment('APPLE_MERCHANT_ID', defaultValue: 'merchant.com.meshaal.meshaalcenter');
